2242678e4d5954f65cb85e31fb8365cd379c5bec
Xihai Luo <xihai.luo01@gmail.com>
2022-02-14 22:40:41 -0500

    add list of friends and ages

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ListScreen = function () {
+  const friends = [
+    { name: "Friend #1", age: "20" },
+    { name: "Friend #2", age: "45" },
+    { name: "Friend #3", age: "32" },
+    { name: "Friend #4", age: "27" },
+    { name: "Friend #5", age: "53" },
+    { name: "Friend #6", age: "30" },
+    { name: "Friend #7", age: "27" },
+    { name: "Friend #8", age: "65" },
+    { name: "Friend #9", age: "18" },
+  ];
+  return (
+    <FlatList
+      keyExtractor={(friend) => friend.name}
+      data={friends}
+      renderItem={({ item }) => {
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 40,
+  },
+});
+
+export default ListScreen;
