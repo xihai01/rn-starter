c76b4e6bd3624590eb28b27e78013ee4a7365fdb
Xihai Luo <xihai.luo01@gmail.com>
2022-02-15 22:17:46 -0500

    add image score text

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,11 +2,12 @@ import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = function (props) {
-  const { title, imageSource } = props;
+  const { title, imageSource, imageScore } = props;
   return (
     <View>
       <Image source={imageSource} />
       <Text>{title}</Text>
+      <Text>Image Score - {imageScore}</Text>
     </View>
   );
 };
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -8,14 +8,17 @@ const ImageScreen = function () {
       <ImageDetail
         title="Forest"
         imageSource={require("../../assets/forest.jpg")}
+        imageScore={9}
       />
       <ImageDetail
         title="Beach"
         imageSource={require("../../assets/beach.jpg")}
+        imageScore={7}
       />
       <ImageDetail
         title="Mountain"
         imageSource={require("../../assets/mountain.jpg")}
+        imageScore={4}
       />
     </View>
   );
