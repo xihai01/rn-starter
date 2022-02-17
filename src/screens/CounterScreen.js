4e35c5edb16e491ae045aa9eb27e53e78f21cd77
Xihai Luo <xihai.luo01@gmail.com>
2022-02-16 22:05:06 -0500

    implement simple counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,23 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const CounterScreen = function () {
+  const [counter, setCounter] = useState(0);
   return (
     <View>
-      <Text>Current Count:</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Text>Current Count: {counter}</Text>
     </View>
   );
 };
