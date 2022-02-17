7e99edc2e654ba0c0d92a3e88af2fc1180b64a67
Xihai Luo <xihai.luo01@gmail.com>
2022-02-16 22:24:45 -0500

    add list of colors

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,11 +1,26 @@
-import React from "react";
-import { View, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Button, FlatList } from "react-native";
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
   return (
     <View>
-      <Button title="Add a Color" />
-      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
+      <Button
+        title="Add a Color"
+        onPress={() => {
+          setColors([...colors, randomRgb()]);
+        }}
+      />
+
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ height: 100, width: 100, backgroundColor: item }} />
+          );
+        }}
+      />
     </View>
   );
 };
