046866f9cc43f3be98f45ff413e851fa91068483
Xihai Luo <xihai.luo01@gmail.com>
2022-02-17 23:07:08 -0500

    implement color demo

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const ColorCounter = (props) => {
+  const { color, onIncrease, onDecrease } = props;
+  return (
+    <View>
+      <Text>{color}</Text>
+      <Button title={`Increase ${color}`} onPress={onIncrease} />
+      <Button title={`Decrease ${color}`} onPress={onDecrease} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorCounter;
